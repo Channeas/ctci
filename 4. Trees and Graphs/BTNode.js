d0f06b2b34d5c906814c077f8829647e5107203e
Johannes <johannes.idarsson@gmail.com>
2021-01-19 09:48:03 +0100

    added parent references to the BinaryTree class

--- 4. Trees and Graphs/BTNode.js
+++ 4. Trees and Graphs/BTNode.js
@@ -1,8 +1,9 @@
 // Implementation of a binary tree
 export default class BTNode {
-    constructor(val) {
+    constructor(val, parent) {
         this.left = null;
         this.right = null;
         this.val = val;
+        this.parent = parent;
     }
 }
--- 4. Trees and Graphs/BinaryTree.js
+++ 4. Trees and Graphs/BinaryTree.js
@@ -3,15 +3,13 @@ import BTNode from "./BTNode.js";
 export default class BinaryTree extends BTNode {
     constructor(nodes) {
         // Make sure there are nodes
-        if (!nodes) {
-            super(null);
+        if (!nodes || nodes.length == 0) {
+            super(null, null);
             return;
         }
 
         // Create the first node
-        if (nodes.length) {
-            super(nodes.shift());
-        }
+        super(nodes.shift(), null);
 
         // Create the child nodes
         const nodeStack = [this];
@@ -25,13 +23,13 @@ export default class BinaryTree extends BTNode {
 
             // Add the left child node (if it exists)
             if (leftVal) {
-                node.left = new BTNode(leftVal);
+                node.left = new BTNode(leftVal, node);
                 nodeStack.push(node.left);
             }
 
             // Add the right child node (if it exists)
             if (rightVal) {
-                node.right = new BTNode(rightVal);
+                node.right = new BTNode(rightVal, node);
                 nodeStack.push(node.right);
             }
         }
